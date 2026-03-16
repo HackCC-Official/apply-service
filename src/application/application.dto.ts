6d6a32258266854c077e2ae283c107d4d2806809
chheangg <balldremz@gmail.com>
2026-03-16 02:31:32 -0700

    feat: attempted json fix

--- src/application/application.dto.ts
+++ src/application/application.dto.ts
@@ -1,15 +1,12 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsArray, IsEnum, IsOptional, isString, IsString, IsUUID, ValidateNested } from "class-validator";
+import { IsArray, IsEnum, IsNumber, IsOptional, isString, IsString, IsUUID, ValidateNested } from "class-validator";
 import { Status } from "./status.enum";
 import { Transform, Type } from "class-transformer";
-import { SubmissionResponseDto } from "src/submission/submission.response-dto";
-import { SubmissionRequestDto } from "src/submission/submission.request-dto";
 import { AccountDTO } from "src/account/account.dto";
 import { Question } from "src/question/question.entity";
 import { ApplicationType } from "./application.entity";
 
 class SubmisisonApplicationDTO {
-  @IsString()
+  @IsNumber()
   @IsOptional()
   questionId?: number;
   @IsOptional()
@@ -36,7 +33,7 @@ export class ApplicationResponseDTO {
   @IsUUID()
   reviewerId?: string;
 
-  @Transform(({ value }) => JSON.parse(value))
+  @Transform(({ value }) => typeof value === "string" ? JSON.parse(value) : value)
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => SubmisisonApplicationDTO)
@@ -54,7 +51,7 @@ export class ApplicationRequestDTO {
   @IsUUID()
   userId: string;
 
-  @Transform(({ value }) => JSON.parse(value))
+  @Transform(({ value }) => typeof value === "string" ? JSON.parse(value) : value)
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => SubmisisonApplicationDTO)
